b0dc697640d03a159c9202e0b5a05bac9937e74c
Hossy <Hossy@users.noreply.github.com>
2022-10-04 22:38:35 -0500

    testing SetKeepAlive

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -e
 apk add --update go build-base git mercurial ca-certificates
+cd /tmp
+git clone https://github.com/Hossy/looplab-logspout-logstash.git
+cp -rf looplab-logspout-logstash/src-override/* /src
 cd /src
+go mod edit -replace invalid.url/looplab/logspout-logstash=/tmp/looplab-logspout-logstash
 go build -ldflags "-X main.Version=$1" -o /bin/logspout
 apk del go git mercurial build-base
 rm -rf /root/go /var/cache/apk/*
