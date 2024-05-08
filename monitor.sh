d6d2168c33d1b7bd38cdf90be9cf05c83bf64179
Hossy <Hossy@users.noreply.github.com>
2024-05-07 21:46:50 -0500

    Improve logic separating handling of not running, no established connections, and time_wait monitoring; Add LSHC_CHECKINTERVAL, LSHC_LAUNCHDELAY, and LSHC_NOACTION options; Improved startup sequence to wait for first connection

--- monitor.sh
+++ monitor.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-debug="${LSHC_DEBUGOUTPUT:-0}"
-killlspid="${LSHC_KILLNOTDIE:-true}"
-noaction="${LSHC_NOACTION:-false}"
-launchdelay="${LSHC_RESTARTDELAY:-5}"
-retrylimit="${LSHC_RETRYLIMIT:-150}"
-sleeptime="${LSHC_SLEEPTIME:-0.1s}"
-waitlimit="${LSHC_WAITLIMIT:-3}"
-waitmonitoring="${LSHC_WAITMONITORING:-true}"
-waitsleep="${LSHC_WAITSLEEP:-90s}"
+checkinterval="${LSHC_CHECKINTERVAL:-1}"        # Sleep interval between normal checks
+debug="${LSHC_DEBUGOUTPUT:-0}"                  # Set to 1 to enable
+killlspid="${LSHC_KILLNOTDIE:-true}"            # Anything not 'true' disables restarts; Terminates entrypoint instead
+launchdelay="${LSHC_LAUNCHDELAY:-0s}"           # Launch delay to start/resume monitoring
+noaction="${LSHC_NOACTION:-false}"              # Set to true to enable
+restartdelay="${LSHC_RESTARTDELAY:-5}"          # Restart delay
+retrylimit="${LSHC_RETRYLIMIT:-150}"            # Number of times to recheck send-Q
+sleeptime="${LSHC_SLEEPTIME:-0.1s}"             # Sleep time in between send-Q rechecks
+waitlimit="${LSHC_WAITLIMIT:-3}"                # Limit of TIME_WAIT connections before triggering restart
+waitmonitoring="${LSHC_WAITMONITORING:-true}"   # Anything not 'true' disables TIME_WAIT connection monitoring
+waitsleep="${LSHC_WAITSLEEP:-90s}"              # Restart delay for TIME_WAIT connection monitoring (in addition to restartdelay)
 
 getlssendq () {
     #/bin/logspout pid
@@ -24,15 +26,17 @@ getlssendq () {
                     [ "${debug}" = "2" ] && >&2 echo "lswaits=${lswaits}"
                     if [ -n "${lswaits}" ] && [ "${lswaits}" -ge "${waitlimit}" ]; then
                         [ "${debug}" = "1" ] && >&2 echo "Changing lssendq from ${lssendq} to 'dead' due to lswaits=${lswaits}"
-                        lssendq="dead"
+                        lssendq="time_wait"
                         sleep "${waitsleep}"
                     fi
                 fi
             fi
+        else
+            echo "no established connections"
         fi
         echo "${lssendq}"
     else
-        echo "dead"
+        echo "not running"
     fi
 }
 
@@ -40,7 +44,7 @@ checklogspout () {
     lssendq=$(getlssendq)
     old_lssendq=${lssendq}
     n=0
-    while [ "${lssendq}" != "0" ] && [ $n -lt "${retrylimit}" ] && [ "${lssendq}" != "dead" ] && [ "${old_lssendq}" -le "${lssendq}" ]; do
+    while [ "${lssendq}" != "0" ] && [ $n -lt "${retrylimit}" ] && [ "${lssendq}" != "not running" ] && [ "${lssendq}" != "time_wait" ] && [ -n "${lssendq}" ] && [ "${old_lssendq}" -le "${lssendq}" ]; do
         sleep "${sleeptime}"
         # [ "${debug}" = "1" ] && echo -n +
         [ "${debug}" = "1" ] && printf '+'
@@ -49,8 +53,22 @@ checklogspout () {
         n=$(( n + 1 ))
     done
 
-    if [ "${lssendq}" = "0" ] || [ "${old_lssendq}" -gt "${lssendq}" ]; then
+    if [ "${lssendq}" = "0" ]; then
         return
+    fi
+    if [ -n "${old_lssendq}" ] && [ -n "${lssendq}" ] && expr "$old_lssendq" : '^[0-9]\+$' >/dev/null 2>&1 && expr "$lssendq" : '^[0-9]\+$' >/dev/null 2>&1 && [ "${old_lssendq}" -gt "${lssendq}" ]; then
+        return
+    fi
+
+    if [ "${lssendq}" != "not running" ]; then
+        printf 'Found logspout not running.  '
+        if [ "${killlspid}" = "true" ]; then
+            echo 'Restarting logspout.'
+            startlogspout
+        else
+            echo 'Exiting'
+            exit
+        fi
     fi
 
     # echo -n 'Timed out waiting for logspout to send data.  '
@@ -65,7 +83,7 @@ checklogspout () {
         lspid=$(pgrep /bin/logspout)
         if [ -n "${lspid}" ]; then
             kill "${lspid}"
-            sleep "${launchdelay}"
+            sleep "${restartdelay}"
         fi
         startlogspout
     else
@@ -78,13 +96,19 @@ startlogspout () {
     echo 'Starting logspout'
     # shellcheck disable=SC2086  # Globbing on purpose
     /bin/logspout ${LSHC_LAUNCHARGS} &
+    lspid=$!
+    echo 'Waiting for logspout to establish connection'
+    while ! netstat -Wntp 2>/dev/null | grep "${lspid}/logspout" | grep -q 'ESTABLISHED' ; do
+        sleep "${checkinterval}"
+    done
+    sleep "${launchdelay}"
 }
 
 startlogspout
 
 echo 'Beginning monitor'
 while true; do
-    sleep 1
+    sleep "${checkinterval}"
     [ "${debug}" = "1" ] && echo .
     checklogspout
 done
